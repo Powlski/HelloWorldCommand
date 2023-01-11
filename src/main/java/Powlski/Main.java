a35dc12ca2c22e2482bef0cebc7a3b8d493dccfd
Stephan Daschek <stedas@web.de>
2023-01-11 16:57:00 +0100

    added was ist los

--- src/main/java/Powlski/Main.java
+++ src/main/java/Powlski/Main.java
@@ -3,5 +3,6 @@ package Powlski;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("was ist los!");
     }
 }

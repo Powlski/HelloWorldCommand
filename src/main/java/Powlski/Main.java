69a2eb9940417c6ccd3011db962d9ddbe45a0cbb
Stephan Daschek <stedas@web.de>
2023-01-12 08:43:01 +0100

    add truth

--- src/main/java/Powlski/Main.java
+++ src/main/java/Powlski/Main.java
@@ -3,5 +3,7 @@ package Powlski;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Welt!");
+        System.out.println("anton ist der beste!");
+
     }
 }

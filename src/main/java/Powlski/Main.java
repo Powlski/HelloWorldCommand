6d058bdf15859c9afc1c30022639ac8106c99a5b
Stephan Daschek <stedas@web.de>
2023-01-12 08:34:17 +0100

    changed text to german

--- src/main/java/Powlski/Main.java
+++ src/main/java/Powlski/Main.java
@@ -2,6 +2,7 @@ package Powlski;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Hallo Welt!");
+
     }
 }

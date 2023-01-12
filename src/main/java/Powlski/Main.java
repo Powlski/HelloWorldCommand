ae45951439ade03e286ef2f67fb155af4420e711
Stephan Daschek <stedas@web.de>
2023-01-12 06:49:48 +0100

    add addition

--- src/main/java/Powlski/Main.java
+++ src/main/java/Powlski/Main.java
@@ -2,7 +2,12 @@ package Powlski;
 
 public class Main {
     public static void main(String[] args) {
+
+        int a = 1;
+        int b =  2;
+        int c = a + b;
+
         System.out.println("Hello world!");
-        System.out.println("was ist los!");
+        System.out.println(c);
     }
 }
